6a06e6ca3870e74118701a500a231caf33f2e20e
Charleswanjiku <kangethewanjiku@gmail.com>
2023-04-22 12:30:06 +0300

    basic

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,32 +1,5 @@
-function createIteratorObject(obj) {
-	let i = 0;
-	const keys = object.keys(obj);
-
-	return {
-		next: function() {
-			return i < keys.length ?
-				{ value: obj[keys[i++]], done: false } :
-				{ done: true };
-		}
-	};
+export default function iterateThroughObject(reportWithIterator) {
+  if (!reportWithIterator) {
+    console.log('Placeholder');
+  }
 }
-
-function iterateThroughObject(reportWithIterator) {
-	let result = reportWithIterator.next();
-	let employees = "";
-
-	while (!result.done) {
-		employees += result.value.name + "|";
-		result = reportWithIterator.next();
-	}
-
-	return employees.slice(0, -1); // remove the last |
-}
-
-const employees = {
-	{ name: "John Doe" }
-	{ name: "Guillaume Salva" }
-};
-const iterator = createIteratorObject(employees);
-const employeeNames = iterateThroughObject(iterator);
-console.log(employeeNames);
