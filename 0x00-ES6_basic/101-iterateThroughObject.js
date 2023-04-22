27c726aa2dbfbe78c8297d8b73b34b22b73bf050
Charleswanjiku <kangethewanjiku@gmail.com>
2023-04-22 12:48:18 +0300

    basic

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,13 +1,5 @@
 export default function iterateThroughObject(reportWithIterator) {
-	let employees = '';
-
-	for (const [i, value] of reportWithIterator.entries()) {
-		if (i !== reportWithIterator.length - 1) {
-			employees += `${value} | `;
-		} else {
-			employees += value;
-		}
-	}
-
-	return employees;
+  if (!reportWithIterator) {
+    console.log('Placeholder');
+  }
 }
