17d47926ca84c158c46f868102af81a8979c0dc4
Charleswanjiku <kangethewanjiku@gmail.com>
2023-04-22 13:04:22 +0300

    basic

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,6 +1,13 @@
-//Exporting functions declarations for createIteratorObject
+// create an array to store all the employees
 export default function createIteratorObject(report) {
-  while (report) {
-    console.log('Placeholder');
-  }
+	const employees = [];
+
+	for (const department of Object.values(report)) {
+		for (const employee of department.employees) {
+			employees.push(employee);
+		}
+	}
+
+	// return an iterator for the employees array
+	return employees[Symbol.iterator]();
 }
