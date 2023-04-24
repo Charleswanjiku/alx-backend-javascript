31348d938ec1d4d3ca92f645d7dc1c33be80c3e0
Charleswanjiku <kangethewanjiku@gmail.com>
2023-04-24 20:25:53 +0300

    manipulation

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,7 @@
-function getListStudentIds(students) {
-	if (!Array.isArray(students)) {
-		return [];
-	}
-	return students.map(student => student.id);
+export default function getListStudentIds(students) {
+  // check arg is an array before using map
+  if (Array.isArray(students)) {
+    return students.map((items) => items.id);
+  }
+  return [];
 }
