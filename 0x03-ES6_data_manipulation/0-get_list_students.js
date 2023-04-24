25ad7a7e04aa9c8e2c4fc37109177ba3866938b6
Charleswanjiku <kangethewanjiku@gmail.com>
2023-04-24 19:47:39 +0300

    manupilation

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,5 +1,5 @@
-const getListStudents() {
-	return [
+function getListStudents() {
+	 [
 		{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
 		{ id: 2, firstName: 'James', location: 'Columbia' },
 		{ id: 5, firstName: 'Serena', location: 'San Francisco' },
