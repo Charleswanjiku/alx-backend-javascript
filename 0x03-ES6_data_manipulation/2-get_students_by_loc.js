1a7157a00247333ac17ed8ec38c33a77979b80ca
Charleswanjiku <kangethewanjiku@gmail.com>
2023-04-24 20:38:21 +0300

    manupltn,

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,7 @@
-function getStudentsByLocation(students, city) {
-	return students.filter(student => student.location === city);
+export default function getStudentsByLocation(students, city) {
+  // student and city array are argument passed thru the filter to create a new array
+  if (Object.getPrototypeOf(students) === Array.prototype) {
+    return students.filter((items) => items.location === city);
+  }
+  return [];
 }
